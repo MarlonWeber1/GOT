cc2818e00140c9b6ae1d6b76ab94881564b54cca
MarlonWeber1 <marlonjlweberf@gmail.com>
2022-11-09 16:49:25 -0300

    Implementação tela info personagens + colors + drawable line

---
+++ app/src/main/java/com/example/got/view/detalhes/CharacterInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.got.view.detalhes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.got.R
+
+class CharacterInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_info)
+    }
+}
