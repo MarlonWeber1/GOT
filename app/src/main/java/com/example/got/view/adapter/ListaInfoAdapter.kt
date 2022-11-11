82f802b83be72853f3b7b927ece2e1a47abf225a
MarlonWeber1 <marlonjlweberf@gmail.com>
2022-11-11 14:29:03 -0300

    Implementação Adapter e Application

---
+++ app/src/main/java/com/example/got/view/adapter/ListaInfoAdapter.kt
@@ -0,0 +1,73 @@
+package com.example.got.view.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.example.got.R
+import com.example.got.databinding.ItemCharacterBinding
+import com.example.got.model.Character
+
+class ListaInfoAdapter(): RecyclerView.Adapter<ListaInfoAdapter.ViewHolder>() {
+
+    private val listaCharacter: ArrayList<Character> = arrayListOf()
+
+    inner class ViewHolder(
+        private val binding : ItemCharacterBinding,
+    ):RecyclerView.ViewHolder(binding.root){
+
+        val imageCharacter = itemView.findViewById<ImageView>(R.id.item_character_imagem)
+
+        fun vincula(character: Character){
+            Glide.with(imageCharacter)
+                .load(character.image)
+                .placeholder(loadCircularProgress(imageCharacter.context))
+                .into(imageCharacter)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemCharacterBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.vincula(listaCharacter[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listaCharacter.size
+    }
+
+    private fun loadCircularProgress(context: Context): CircularProgressDrawable {
+        val circularProgressDrawable = CircularProgressDrawable(context)
+        val colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+            ContextCompat.getColor(context, R.color.background_blue),
+            BlendModeCompat.SRC_ATOP
+        )
+
+        circularProgressDrawable.colorFilter = colorFilter
+        circularProgressDrawable.strokeWidth = 10f
+        circularProgressDrawable.centerRadius = 60f
+        circularProgressDrawable.start()
+
+        return circularProgressDrawable
+    }
+
+    fun mandaProAdapter(listaMandaAdapter: List<Character>){
+        val oldRangeItem = listaCharacter.size
+        val newRangeItem = listaMandaAdapter.size
+
+        listaCharacter.clear()
+        listaCharacter.addAll(listaMandaAdapter)
+
+        notifyItemRangeInserted(oldRangeItem, newRangeItem)
+    }
+
+}
