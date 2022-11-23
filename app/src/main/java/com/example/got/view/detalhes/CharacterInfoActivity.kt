0fa445298e47efe7f0bac6905f9fb0bc47e0fada
MarlonWeber1 <marlonjlweberf@gmail.com>
2022-11-23 17:15:39 -0300

    Começo da implementação da tela edit character

--- app/src/main/java/com/example/got/view/detalhes/CharacterInfoActivity.kt
+++ app/src/main/java/com/example/got/view/detalhes/CharacterInfoActivity.kt
@@ -1,17 +1,19 @@
 package com.example.got.view.detalhes
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.got.R
 import com.example.got.databinding.ActivityCharacterInfoBinding
 import com.example.got.model.Character
 import com.example.got.utils.loadImage
+import com.example.got.view.EditActivity
 
 class CharacterInfoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCharacterInfoBinding
+    private lateinit var btnEdit: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +30,10 @@ class CharacterInfoActivity : AppCompatActivity() {
         binding.infoTitle.text = result?.title
         binding.infoFamily.text = result?.family
 
+        btnEdit = findViewById(R.id.button_info_activity)
+        btnEdit.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            startActivity(intent)
+        }
     }
-
 }
